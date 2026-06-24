94000987115b9216620a0eaf7ac3850f8ee8f1f4
KhaiBaDao <khai271609@gmail.com>
2026-06-24 21:54:42 +0700

    meomeo

--- On_Class/Mang.c
+++ On_Class/Mang.c
@@ -1,70 +1,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <time.h>
+#define MAX 100
+ 
+/*
+ 
+Xây dựng một mô hình meunu với các chức năng sau
+ 
+1. Input: đầu vào
+Trong đầu vào sẽ nhập số lượng kí tự của mảng (tối đa 100)
+2. Display Array: Hiển thị mảng
+3. Search for a number
+4. Caculate statistics (Sum, Average, Max, Min)
+5. Exit
+ 
+*/
+ 
+// Variable
+ 
+int a[MAX];
+int n;
+int Sum = 0;
+int ma = INT_MIN, mi = INT_MAX;
+int choice;
+int check = 0;
+int gt;
+int check_sort = 0;
+ 
+// ------------------------------------------- //
 
-
-void linear_sort(int a[], int n){
-	for(int i = 0 ; i < n - 1 ; ++i){
-		int cs = i;
-		for(int j = i + 1 ; j < n ; ++j){
-			if(a[cs] > a[j]) cs = j;
-		}
-		if(cs > i){
-			int tmp = a[i];
-			a[i] = a[cs];
-			a[cs] = tmp;
-		}
-	}
-}
-
-int found(int a[], int n, int gt){
-	int l = 0, r = n - 1;
-	while(l <= r){
-		int mid = l + (r - l)/2;
-		if(a[mid] == gt) return mid;
-		if(a[mid] > gt) r = mid - 1;
-		else l = mid + 1;
+int finding(int a[], int n, int gt){
+	for(int i = 0 ; i < n ; ++i){
+		if(a[i] == gt) return i;
 	}
 	return -1;
 }
 
+
 void menu()
 {
-	printf("--- Menu ---\n");
+	printf("\n ---- Menu ----\n");
 	printf("1. Input Data\n");
 	printf("2. Display Array\n");
 	printf("3. Search for a number\n");
 	printf("4. Calculate Statistics (Sum, Average, Max, Min)\n");
 	printf("5. Exit!\n");
 }
-
+ 
+// 1.
+ 
+void input_data(int a[], int *n){
+	do
+	{
+		printf("Enter the number of elements (1 - 100): ");
+		scanf("%d", n);
+		if(*n < 1 || *n > 100) printf("Invailed size!. Please enter a number betwwen 1 and 100.\n");
+	}
+	while(*n < 1 || *n > 100);
+ 
+	printf("Enter %d numbers: \n", *n);
+	for(int i = 0 ; i < *n ; ++i){
+		printf("Number %d: ", i + 1);
+		scanf("%d", (a + i));
+	}
+}
+ 
+// 2.
+ 
 void display_array(int a[], int n){
 	printf("\nArray elements: ");
 	for(int i = 0 ; i < n ; ++i){
 		printf("%d ", *(a + i));
 	}
+	printf("\n");
 }
-
-void Search_for_a_number(int a[], int n, int gt){
-	if(found(a, n, gt) != -1){
-		printf("The number %d is found at index %d.", gt, found(a, n, gt));
-	}else printf("The number %d is not found in the array!", gt);
+ 
+// 3.
+ 
+void search_for_a_number(int a[], int n, int *gt){
+	
+	printf("Enter the number you want to search for: ");
+	scanf("%d", gt);
+	if(finding(a, n, *gt) != -1){
+		printf("The number %d is found at index %d.\n", *gt, finding(a, n, *gt));
+	}else printf("The number %d is not found in the array!", *gt);
 }
-
-
-
+ 
+// 4. 
+ 
+void calculate_statistics(int a[], int n){
+	for(int i = 0 ; i < n ; ++i){
+		Sum += a[i];
+		if(a[i] > ma) ma = a[i];
+		if(a[i] < mi) mi = a[i];
+	}
+ 
+	printf("Sum: %d\n", Sum);
+	printf("Average: .2%f\n", (double)Sum/n);
+	printf("Maximum: %d\n", ma);
+	printf("Miximum: %d\n", mi);
+}
+ 
+ 
 int main()
 {
-	menu();
-	int n; scanf("%d", &n);
-	int a[n];
-	for(int i = 0 ; i < n ; ++i){
-		scanf("%d", (a + i));
+	while(1){
+		menu();
+		do
+		{
+			printf("Enter your choice: ");
+			scanf("%d", &choice);
+			if(choice < 1 || choice > 5) printf("Invailed value. Please try again");
+		}while(choice < 1 || choice > 5);
+ 
+		if(choice == 1) input_data(a, &n);
+		if(choice == 2) display_array(a, n);
+		if(choice == 3) search_for_a_number(a, n, &gt);
+		if(choice == 4) calculate_statistics(a, n);
+		if(choice == 5){
+			printf("Exiting the program!");
+			break;
+		}
 	}
-//	linear_sort(a, n);
-	int res; scanf("%d", &res);
-	Search_for_a_number(a, n, res);
-	
+ 
 	return 0;
 }
